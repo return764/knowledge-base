9b367b7555387c1800869e542b40cfd63c9bb07f
return764 <543851436@qq.com>
2025-04-17 16:42:14 +0800

    feat: support password type for input cmp

--- src/components/basic/form/components/Input.tsx
+++ src/components/basic/form/components/Input.tsx
@@ -1,6 +1,7 @@
-import {ChangeEvent, HTMLInputTypeAttribute, useMemo} from 'react';
+import {ChangeEvent, HTMLInputTypeAttribute, useMemo, useState} from 'react';
 import { Input as HeadlessInput } from '@headlessui/react'
 import {OnChangeAndValue} from "../interface.ts";
+import { LuEye, LuEyeOff } from "react-icons/lu";
 
 type InputProps = {
     name?: string,
@@ -11,7 +12,8 @@ type InputProps = {
 } & OnChangeAndValue
 
 function Input(props: InputProps) {
-    const {size = "default", placeholder} = props
+    const {size = "default", placeholder, type} = props
+    const [showPassword, setShowPassword] = useState(false)
 
     const inputClass = useMemo(() => {
         switch (size) {
@@ -28,13 +30,36 @@ function Input(props: InputProps) {
         props.onChange?.(e.currentTarget.value)
     }
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const inputType = type === 'password' && showPassword ? 'text' : type
+
     return (
-        <HeadlessInput
-            value={props.value ?? ""}
-            onChange={handleChange}
-            type={props.type}
-            placeholder={placeholder}
-            className={`${inputClass} bg-gray-50 border border-[#CFD1E8] text-gray-900 rounded-lg focus:ring-0 focus:outline-0 focus:border-[#cfd0e8] block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} name={props.name}/>
+        <div className="relative">
+            <HeadlessInput
+                value={props.value ?? ""}
+                onChange={handleChange}
+                type={inputType}
+                placeholder={placeholder}
+                className={`${inputClass} bg-gray-50 border border-[#CFD1E8] text-gray-900 rounded-lg focus:ring-0 focus:outline-0 focus:border-[#cfd0e8] block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                name={props.name}
+            />
+            {type === 'password' && (
+                <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                >
+                    {showPassword ? (
+                        <LuEye/>
+                    ) : (
+                        <LuEyeOff/>
+                    )}
+                </button>
+            )}
+        </div>
     );
 }
 
